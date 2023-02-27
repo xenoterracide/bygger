7c49f15549fcacd621feb95c31dc05e62682bb95
Caleb Cushing <xenoterracide@gmail.com>
2023-02-27 16:41:16 -0500

    add checkstyle files

--- src/test/java/com/xenoterracide/bygger/ClassAnnotationTest.java
+++ src/test/java/com/xenoterracide/bygger/ClassAnnotationTest.java
@@ -1,18 +1,20 @@
 package com.xenoterracide.bygger;
 
-import static com.google.testing.compile.Compiler.javac;
-
 import com.google.testing.compile.JavaFileObjects;
 import org.junit.jupiter.api.Test;
+
+import static com.google.testing.compile.Compiler.javac;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ClassAnnotationTest {
   @Test
   void testImmutable() {
-    javac().compile(
+    var comp = javac().compile(
       JavaFileObjects.forSourceLines(
         "cxb.Example",
         "package cxb;"
       )
     );
+    assertThat( comp.errors() ).isEmpty();
   }
 }
