33e247d8f48711918fcf1aad7f17276923370c29
Caleb Cushing <xenoterracide@gmail.com>
2023-08-08 13:32:02 -0400

    fix use of jvmArgs api

--- buildSrc/src/main/kotlin/our.java-test.gradle.kts
+++ buildSrc/src/main/kotlin/our.java-test.gradle.kts
@@ -33,11 +33,9 @@ tasks.test.configure {
   useJUnitPlatform()
 
   // See: https://github.com/google/compile-testing/issues/222
-  jvmArgs = listOf(
-    "--add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
-    "--add-exports=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
-    "--add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED"
-  )
+  jvmArgs("--add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED")
+  jvmArgs("--add-exports=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED")
+  jvmArgs("--add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED")
 
   testLogging {
     lifecycle {
